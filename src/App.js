e7d186c7a14d4ce1864bc481f259836f0d7450b9
Elena <60514098+Elena-GHub@users.noreply.github.com>
2021-01-02 21:39:46 +0100

    Initial setup and a couple of minor editing
    "Hello World" app first steps included in the ReactJS-Reduct course by Nicolás Schürmann at Udemy

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Bienvenid@s a React</h1>
+        </header>
+        <h3 className="App-intro">
+          ¡Hola, Mundo!
+        </h3>
+      
+    </div>
+  );
+}
+
+export default App;
