625a781cae8c2975445b4ef1e292221691b4398e
Elena-GHub <cabjelen@yahoo.es>
2021-01-16 20:32:41 +0100

    chanded header css style to component inline style (video 20)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 import logo from '../logo.svg';
 
+const styles = {
+    header: {
+        backgroundColor: '#282c34',
+        minHeight: '25vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 'calc(10px + 2vmin)',
+        color: 'white'
+    }
+}
 export default class Header extends Component {
     handleClick = () => {
         const { handleClick, miau} = this.props
@@ -9,7 +21,7 @@ export default class Header extends Component {
     render() {
         const { miau, handleClick } = this.props
         return (
-            <header className="App-header">
+            <header style={ styles.header }>
                 <img onClick={this.handleClick} src={logo} className="App-logo" alt="logo" />
                 <h1 className="App-title">{miau}</h1>
             </header>
