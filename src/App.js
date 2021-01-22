9f564ada9d90dcac06c972c3ac560af845cc6033
Elena-GHub <cabjelen@yahoo.es>
2021-01-22 18:46:15 +0100

    back to plain Hello World prior to startcreating a form - Section 5 video 23

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React, { Component } from 'react'
-import Header from './components/Header'
-import P from './components/P'
-import './App.css';
 
 class App extends Component {
-  state = {
-    miau: 'Welcome to miau'
-  }
-
-  changeStateText = () => {
-    this.setState({ miau: 'Hello World!'})
-  }
-  handleClick = text => {
-    console.log(text)
-  }
   render() {
-    const { miau } = this.state
-    const text = 'Welcome miau'
-    const guau = 'Welcome guau'
     return (
       <div className="App">
-          <Header miau={miau} handleClick={this.handleClick} />
-          <P onClick={this.changeStateText}>
-            { miau }  
-          </P>         
+          Hello World!    
       </div>
     )
   }
