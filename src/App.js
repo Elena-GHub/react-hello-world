b93954b65dc2b3a78274cdb516a14e7316b23c56
Elena-GHub <cabjelen@yahoo.es>
2021-01-25 18:31:55 +0100

    added fetch GET & POST constructor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react'
 import SimpleForm from './components/SimpleForm'
 
 class App extends Component {
+  constructor() {
+    super()
+    // GET, POST, PUT, PATCH, DELETE
+    // fetch('http://jsonplaceholder.typicode.com/users')
+    // .then(x => x.json())
+    // .then(x => console.log(x))
+    fetch('http://jsonplaceholder.typicode.com/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'applicaton/json'
+      },
+      body: JSON.stringify({
+        name: 'Pepe Lomo',
+        username: 'Pepito',
+      })
+    }).then(x => x.json())
+    .then(x => console.log(x))
+  }
   render() {
     return (
       <div className="App">
